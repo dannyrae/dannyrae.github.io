b2b76205732cd0c309689da3b4b50643f70567ee
Emmanuel Asuquo <emmanueldan0920@gmail.com>
2023-01-20 16:22:58 +0100

    responsive for all screens

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const open = document.querySelector('.open')
+const close = document.querySelector('.close')
+const ul = document.querySelector('ul')
+const body = document.querySelector('body')
+ 
+
+open.addEventListener('click', () => {
+    ul.classList.add('show')
+    body.style.overflowY = 'hidden'
+})
+
+close.addEventListener('click', () => {
+    ul.classList.remove('show')
+    body.style.overflowY = 'auto'
+})
